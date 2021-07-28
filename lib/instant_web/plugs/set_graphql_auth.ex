3f07a21dcce1faa16536837c03b914ffe63ff77e
ryuzaki <ryuzaki>
2021-07-28 16:13:09 +0530

    protect graphql

--- lib/instant_web/plugs/set_graphql_auth.ex
+++ lib/instant_web/plugs/set_graphql_auth.ex
@@ -8,26 +8,25 @@ defmodule InstantWeb.Plugs.SetGraphqlAuth do
   end
 
   def call(conn, _params) do
-    conn
-    # user_id = Plug.Conn.get_session(conn, :current_user_id)
+    user_id = Plug.Conn.get_session(conn, :current_user_id)
 
-    # if user_id do
-    #   cond do
-    #     user = UserRepo.get_by_id(user_id) ->
-    #       conn =
-    #         conn
-    #         |> assign(:current_user, user)
+    if user_id do
+      cond do
+        user = UserRepo.get_by_id(user_id) ->
+          conn =
+            conn
+            |> assign(:current_user, user)
 
-    #       Absinthe.Plug.put_options(conn, context: %{current_user: user})
+          Absinthe.Plug.put_options(conn, context: %{current_user: user})
 
-    #     true ->
-    #       conn
-    #       |> assign(:current_user, nil)
-    #       |> assign(:user_signed_in?, false)
-    #   end
-    # else
-    #   send_resp(conn, 401, Constants.not_authenticated())
-    #   conn |> halt()
-    # end
+        true ->
+          conn
+          |> assign(:current_user, nil)
+          |> assign(:user_signed_in?, false)
+      end
+    else
+      send_resp(conn, 401, Constants.not_authenticated())
+      conn |> halt()
+    end
   end
 end
